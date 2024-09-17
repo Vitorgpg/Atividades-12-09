3b496ded41aa2d29845f034afbae6bf9254f58d3
Vitor Gabriel Pinheiro Gomes <141376701+Vitorgpg@users.noreply.github.com>
2024-09-16 22:15:22 -0300

    atividade que estava faltando

---
+++ Positivo-negativo/positivo.js
@@ -0,0 +1,9 @@
+var numero = parseInt(prompt("Digite um número inteiro:"));
+
+if (numero < 0) {
+    console.log("Negativo");
+} else if (numero > 0 ){
+    console.log("Positivo");
+} else {
+    console.log("Zero");
+}
